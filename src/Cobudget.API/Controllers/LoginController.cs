161c1656b24876bfcea0b471e830374d44337317
keven.tavares <keven.tavares@maxdata.com.br>
2025-07-23 14:59:15 -0300

    Login endpoint

---
+++ src/Cobudget.API/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using CoBudget.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cobudget.Api.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        //[FromServices] IRegisterUserUseCase useCase, [FromBody] RequestRegisterUserJson request
+        )
+    {
+        //var response = await useCase.Execute(request);
+
+        return Created(string.Empty, string.Empty);
+    }
+}
